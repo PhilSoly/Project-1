453a30a6157b15091039496682c2efcfdbdbcb42
Chris Nasser <NsirhC@users.noreply.github.com>
2016-07-22 21:14:51 -0700

    Delete UpdateAuto.java

--- UpdateAuto.java
+++
@@ -1,9 +0,0 @@
-package adapter;
-
-public interface UpdateAuto {
-	
-	public void updateOptionSetName(String ModelName, String OptionSetname, String newname);
-
-	public boolean updateOptionPrice(String Modelname, String OptionName, String Option, float newprice);
-	
-}
