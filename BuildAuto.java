3c925ed1614b14b1bd5e0ff4b18f3f6566f89b2b
Chris Nasser <NsirhC@users.noreply.github.com>
2016-07-22 21:13:38 -0700

    Delete BuildAuto.java

--- BuildAuto.java
+++
@@ -1,7 +0,0 @@
-package adapter;
-import server.AutoServer;
-import client.AutoClient;
-
-public final class BuildAuto extends proxyAutomobile implements CreateAuto, UpdateAuto, FixAuto, GetChoice, EditAuto, AutoServer, AutoClient{
-	
-}
