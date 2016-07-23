aea25742e8ac89653950c6c43dfed1dde92f51e4
Chris Nasser <NsirhC@users.noreply.github.com>
2016-07-22 21:14:16 -0700

    Delete GetChoice.java

--- GetChoice.java
+++
@@ -1,8 +0,0 @@
-package adapter;
-
-public interface GetChoice {
-	
-	public void getOptionChoice(String modelName, String setName);
-	
-	public void setOptionChoice(String modelName, String setName, String optionName);
-}
