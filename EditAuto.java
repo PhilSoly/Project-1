ddaedc447c638bc4946b7b437e35790c15a30eb8
Chris Nasser <NsirhC@users.noreply.github.com>
2016-07-22 21:14:05 -0700

    Delete EditAuto.java

--- EditAuto.java
+++
@@ -1,8 +0,0 @@
-package adapter;
-
-public interface EditAuto {
-	
-	//public void editOptionName(String ModelName, String OptionSetname, String newname, String oldname);
-
-	public void editOptionPrice(String Modelname, String OptionName, String Option, float newprice, int threadno);
-}
