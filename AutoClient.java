427ca2549e3ce87f643ccbeebac102f0f3ee9b13
Chris Nasser <NsirhC@users.noreply.github.com>
2016-07-22 21:13:33 -0700

    Delete AutoClient.java

--- AutoClient.java
+++
@@ -1,9 +0,0 @@
-package client;
-import java.io.IOException;
-import java.util.Properties;
-
-public interface AutoClient {
-
-	public Properties uploadProp(String filename) throws IOException;
-	
-}
